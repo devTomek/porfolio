45d99b3901c4d9a8cc046049bdff73231c47e449
Tomasz Chybzinski <tomasz.chybzinski@gmail.com>
2018-10-24 22:06:29 +0200

    Video controls make it visible. Video css removed.

--- src/components/homePage/sectionThree/SectionThree.js
+++ src/components/homePage/sectionThree/SectionThree.js
@@ -5,10 +5,8 @@ class SectionThree extends Component {
   render() {
     return (
       <div className="section-three">
-        {/*todo probably wrong PNG*/}
-        <video poster="../../../assets/manTypingFrame.PNG" autoPlay muted loop>
-          {/*todo wrong src*/}
-          <source src="../../../assets/manTyping.mp4" type="video/mp4"/>
+        <video poster="../../../assets/manTypingFrame.PNG" autoPlay muted loop controls>
+          <source src="../../../assets/manTyping.mp4"/>
           Your browser does not support the video tag.
         </video>
         <p>This awesome videos are possible because of HTML5</p>
