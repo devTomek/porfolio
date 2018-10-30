fe715832c451fc9ac5001253dd28725eef889cde
Tomasz Chybzinski <tomasz.chybzinski@gmail.com>
2018-10-30 20:25:56 +0100

    Finish SectionThree

--- src/components/homePage/sectionThree/SectionThree.jsx
+++ src/components/homePage/sectionThree/SectionThree.jsx
@@ -5,11 +5,10 @@ import manTypingFrame from "../../../assets/manTypingFrame.PNG"
 
 const SectionThree = () =>
   <div className="section-three">
-    <video poster={manTypingFrame} autoPlay muted loop>
+    <video poster={manTypingFrame} autoPlay muted loop width="100%">
       <source src={manTyping}/>
       Your browser does not support the video tag.
     </video>
-    {/*todo find ohter opacity way or make it 100% video height*/}
     <div className="text-wrapper">
       <p>This awesome videos are possible because of HTML5</p>
       <p>Check all the technologies I used to create this website below</p>
