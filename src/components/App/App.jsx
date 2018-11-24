55fecc2b24696400bfe0049e4fd4e967686bd966
Tomasz Chybzinski <tomasz.chybzinski@gmail.com>
2018-11-24 16:05:12 +0100

    refactor

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,6 +4,7 @@ import TransitionLayer from "../HomePage/TransitionLayer/TransitionLayer";
 import VideoLayer from "../HomePage/VideoLayer/VideoLayer";
 import WelcomeLayer from "../HomePage/WelcomeLayer/WelcomeLayer";
 import TechnologyLayer from "../HomePage/TechnologyLayer/TechnologyLayer";
+import SocialMediaLayer from "../HomePage/SocialMediaLayer/SocialMediaLayer";
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
         <TransitionLayer/>
         <VideoLayer/>
         <TechnologyLayer/>
+        <SocialMediaLayer/>
       </React.StrictMode>
     )
   }
--- src/components/HomePage/VideoLayer/VideoLayer.jsx
+++ src/components/HomePage/VideoLayer/VideoLayer.jsx
@@ -5,13 +5,17 @@ import manTypingFrame from "../../../assets/manTypingFrame.PNG"
 
 const VideoLayer = () =>
   <div className="video-layer">
-    <video poster={manTypingFrame} autoPlay muted loop width="100%">
-      <source src={manTyping}/>
-      Your browser does not support the video tag.
-    </video>
-    <div className="text-wrapper">
-      <p>This awesome videos are possible because of HTML5</p>
-      <p>Check all the technologies I used to create this website below</p>
+    <div className="video">
+      <video poster={manTypingFrame} autoPlay muted loop width="100%">
+        <source src={manTyping}/>
+        Your browser does not support the video tag.
+      </video>
+    </div>
+    <div className="content-wrapper">
+      <div className="content">
+        <p>This awesome videos are possible because of HTML5</p>
+        <p>Check all the technologies I used to create this website below</p>
+      </div>
     </div>
   </div>;
 
