10733e1441f7881c699a5056bed918ca654343cc
Tomasz Chybzinski <tomasz.chybzinski@gmail.com>
2018-12-23 17:20:35 +0100

    redesigned landing

--- src/components/HomePage/WelcomeLayer/WelcomeLayer.jsx
+++ src/components/HomePage/WelcomeLayer/WelcomeLayer.jsx
@@ -3,23 +3,13 @@ import './WelcomeLayer.scss';
 
 const WelcomeLayer = () =>
   <div className="welcome-layer">
-    <div className="content-wrapper">
-      <div className="technologies">
-        <div className="flex-row first-row">
-          <p>JavaScript</p>
-          <p>HTML</p>
-          <p>React</p>
-        </div>
-        <div className="flex-row second-row">
-          <p>Node</p>
-          <p>Redux</p>
-          <p>CSS</p>
-        </div>
-      </div>
-      <div className="avatar-wrapper">
-        <div className="avatar"/>
-        <p>Tomasz Chybziński</p>
-      </div>
+    <div className="text-wrapper">
+      <p>
+        <div className="author">Tomasz Chybziński</div>
+        Hello!<br />
+        I am a JavaScript Developer<br />
+        I create <span className="changing-word-wrapper">Websites</span>
+      </p>
     </div>
   </div>;
 
