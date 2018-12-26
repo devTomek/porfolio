28c7b4715812ceb4348040b4fbc4ffdd42e5aa3f
Tomasz Chybzinski <tomasz.chybzinski@gmail.com>
2018-12-26 15:34:54 +0100

    Navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,29 +10,28 @@ import {
 } from 'reactstrap';
 import './Header.scss';
 
-const Header = ({ toggle, isOpen, text }) => (
+const Header = ({ toggle, isOpen, text }) => 
   <div className="header-wrapper">
-    <Navbar dark expand="md">
-      <NavbarBrand href="/">{text.brand}</NavbarBrand>
+    <Navbar dark expand="lg">
+      <NavbarBrand href="#">{text.brand}</NavbarBrand>
       <NavbarToggler onClick={toggle} className="navbar-toggler" />
       <Collapse isOpen={isOpen} navbar>
         <Nav className="ml-auto" navbar>
           <NavItem>
-            <NavLink href="/">{text.home}</NavLink>
+            <NavLink href="#">{text.home}</NavLink>
           </NavItem>
           <NavItem>
-            <NavLink href="/">{text.about}</NavLink>
+            <NavLink href="#">{text.about}</NavLink>
           </NavItem>
           <NavItem>
-            <NavLink href="/">{text.projects}</NavLink>
+            <NavLink href="#">{text.projects}</NavLink>
           </NavItem>
           <NavItem>
-            <NavLink href="/">{text.contact}</NavLink>
+            <NavLink href="#">{text.contact}</NavLink>
           </NavItem>
         </Nav>
       </Collapse>
     </Navbar>
-  </div>
-)
+  </div>;
 
 export default Header;
---
+++ src/components/Header/HeaderContainer.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import Header from './Header';
+
+class HeaderContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isOpen: false
+    }
+    this.text = {
+      brand: "TC",
+      home: "Home",
+      about: "About",
+      projects: "Projects",
+      contact: "Contact"
+    }
+    this.prevScroll = 0;
+    this.scrolling = false;
+  }
+
+  componentDidMount() {
+    document.addEventListener('scroll', this.handleScroll)
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('scroll', this.handleScroll)
+  }
+
+  toggle = () => this.setState({
+    isOpen: !this.state.isOpen
+  })
+
+  handleScroll = () => {
+    if (!this.scrolling) {
+      window.requestAnimationFrame(() => {
+        this.toggleNavbar();
+        this.scrolling = false;
+      });
+
+      this.scrolling = true;
+    }
+  }
+
+  toggleNavbar = () => {
+    const currScroll = window.scrollY;
+
+    if (this.prevScroll < currScroll) {
+      if (this.headerRef) {
+        this.headerRef.style.top = '-300px';
+      }
+    } else {
+      if (this.headerRef) {
+        this.headerRef.style.top = 0;
+      }
+    }
+    this.prevScroll = currScroll;
+  }
+
+  render() {
+    return (
+      <div ref={node => this.headerRef = node} className="header-slider">
+        <Header toggle={this.toggle} isOpen={this.state.isOpen} text={this.text} />
+      </div>
+    );
+  }
+}
+
+export default HeaderContainer;
