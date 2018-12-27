30cdfd4e565853469e385307b0f158f6643d49b1
Tomasz Chybzinski <tomasz.chybzinski@gmail.com>
2018-12-27 15:45:09 +0100

    responsive landing, responsive technoloies

--- src/components/HomePage/TechnologyLayer/TechnologyLayer.jsx
+++ src/components/HomePage/TechnologyLayer/TechnologyLayer.jsx
@@ -11,19 +11,19 @@ const TechnologyLayer = () =>
   <div className="technology-layer">
     <div className="content-wrapper">
       <div className="column-wrapper">
-        <div className="technology-wrapper">
+        <div className="technology-wrapper html">
           <span className="image">
             <img src={html} alt="html.svg" />
           </span>
           <p>New HTML5 &lt;video/&gt; tag is a great replacement of the old flash technology!</p>
         </div>
-        <div className="technology-wrapper">
+        <div className="technology-wrapper css">
           <span className="image">
             <img src={css} alt="css.svg" />
           </span>
           <p>CSS3 allows to create beautiful styles but do not forget that CSS can do smooth animations also!</p>
         </div>
-        <div className="technology-wrapper">
+        <div className="technology-wrapper sass">
           <span className="image">
             <img src={sass} alt="sass.svg" />
           </span>
@@ -43,7 +43,7 @@ const TechnologyLayer = () =>
             <img src={react} alt="react.svg" />
           </span>
         </div>
-        <div className="technology-wrapper">
+        <div className="technology-wrapper redux">
           <p>Thanks to Redux the data is being passed in a more efficient and cleaner way!</p>
           <span className="image">
             <img src={reduxIcon} alt="redux.svg" />
