162e1a00eb954fb8641bc1ad1fa42cda861a44ec
Tomasz Chybzinski <tomasz.chybzinski@gmail.com>
2018-12-28 15:34:05 +0100

    responsive VideoLayer

--- src/components/HomePage/VideoLayer/VideoLayer.jsx
+++ src/components/HomePage/VideoLayer/VideoLayer.jsx
@@ -4,16 +4,14 @@ import manTyping from "../../../assets/manTyping.mp4";
 import manTypingFrame from "../../../assets/manTypingFrame.PNG"
 
 const VideoLayer = () =>
-  <div className="video-layer">
-    <div className="video">
+  <div>
+    <div className="video-layer">
       <video poster={manTypingFrame} autoPlay muted loop width="100%">
-        <source src={manTyping}/>
+        <source src={manTyping} />
         Your browser does not support the video tag.
       </video>
-    </div>
-    <div className="content-wrapper">
-      <div className="content">
-        <p>This awesome videos are possible because of HTML5</p>
+      <div className="text-wrapper">
+        <p>This awesome videos are possible because of <span className="html">HTML5</span></p>
         <p>Check all the technologies I used to create this website below</p>
       </div>
     </div>
