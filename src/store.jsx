a71055de57c32e79830adb5d67b6daa4625a955f
Tomasz Chybzinski <tomasz.chybzinski@gmail.com>
2019-08-08 22:46:19 +0200

    disable redux devtolls in production

--- src/store.jsx
+++ src/store.jsx
@@ -2,5 +2,8 @@ import { createStore } from 'redux';
 import rootReducer from "./rootReducer";
 
 export default function configureStore(initialState = {}) {
-    return createStore(rootReducer);
+    return createStore(
+        rootReducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ && process.env.NODE_ENV === "development" && window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
 }
