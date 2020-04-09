14177b0227773114a02c9c2b22d40fbd137728d5
Tomasz Chybzinski <tomasz.chybzinski@gmail.com>
2020-04-09 21:52:33 +0200

    Updates

---
+++ precache-manifest.9322b03ac33c1659f08f5f8c82ab9747.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "e02cc89ce5189d5c83ec",
+    "url": "/porfolio/static/css/main.07736321.chunk.css"
+  },
+  {
+    "revision": "e02cc89ce5189d5c83ec",
+    "url": "/porfolio/static/js/main.342cafe0.chunk.js"
+  },
+  {
+    "revision": "2a1056f9dbc6e68ef264",
+    "url": "/porfolio/static/js/runtime~main.7912f799.js"
+  },
+  {
+    "revision": "dbeadb64afd009400390",
+    "url": "/porfolio/static/css/2.97f7d1ca.chunk.css"
+  },
+  {
+    "revision": "dbeadb64afd009400390",
+    "url": "/porfolio/static/js/2.866adaf5.chunk.js"
+  },
+  {
+    "revision": "a95af86a15596ab022540d1e9adcc776",
+    "url": "/porfolio/static/media/manTyping.a95af86a.mp4"
+  },
+  {
+    "revision": "279cad615beacfe261aef849d41ae9e4",
+    "url": "/porfolio/static/media/manTypingFrame.279cad61.PNG"
+  },
+  {
+    "revision": "918c32fc6204ac8d5b7dd75b1d685f89",
+    "url": "/porfolio/static/media/html5.918c32fc.svg"
+  },
+  {
+    "revision": "0038f72c9d477b9264b6376ef7b0df1e",
+    "url": "/porfolio/static/media/css3.0038f72c.svg"
+  },
+  {
+    "revision": "a5ab8a67d52562b26d6c9dbef5b7d423",
+    "url": "/porfolio/static/media/sass.a5ab8a67.svg"
+  },
+  {
+    "revision": "3f9ca62fc3b9eb791c709f59c0648d07",
+    "url": "/porfolio/static/media/js.3f9ca62f.svg"
+  },
+  {
+    "revision": "f0793d9f85eb3f0d1c702c14d236ba9e",
+    "url": "/porfolio/static/media/react.f0793d9f.svg"
+  },
+  {
+    "revision": "85772647236e76b8e772c14c092a28ac",
+    "url": "/porfolio/static/media/redux.85772647.svg"
+  },
+  {
+    "revision": "02671a86168d25445fe5587ad7bc55ae",
+    "url": "/porfolio/static/media/manTyping2.02671a86.mp4"
+  },
+  {
+    "revision": "dc8bf72b087b435285863f8b8815046f",
+    "url": "/porfolio/static/media/manTyping2Frame.dc8bf72b.PNG"
+  },
+  {
+    "revision": "dbc6b720661dc1f07e4345176292c879",
+    "url": "/porfolio/static/media/avatar.dbc6b720.jpg"
+  },
+  {
+    "revision": "ce0d246bd75d48e406fd08f2803c67b9",
+    "url": "/porfolio/static/media/sectionOneBackgroundMobile.ce0d246b.jpg"
+  },
+  {
+    "revision": "f7f59a43411e208f82d0d2fefc606ca8",
+    "url": "/porfolio/static/media/sectionOneBackground.f7f59a43.jpg"
+  },
+  {
+    "revision": "1fd589c2feed5fdcc0f7a73a24972e60",
+    "url": "/porfolio/index.html"
+  }
+];
